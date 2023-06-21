fca6611d92255583af4e2b65b5042e5fc3bdb2fe
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-06-22 00:30:21 +0200

    Lesser test: unparse is identical. ast.dump cannot by used

--- tests/test_astnode.py
+++ tests/test_astnode.py
@@ -11,9 +11,9 @@ from tests.common import AstunparseCommonTestCase
 class UnparseTestCase(AstunparseCommonTestCase, unittest.TestCase):
 
     def assertASTEqual(self, ast1, ast2):
-        if ast.dump(ast1) != ast.dump(ast2):
-            print('Fail', ast.dump(ast1), ast.dump(ast2))
-        self.assertEqual(ast.dump(ast1), ast.dump(ast2))
+        if astunparse.unparse(ast1) != astunparse.unparse(ast2):
+            print('Fail', astunparse.unparse(ast1), astunparse.unparse(ast2))
+        self.assertEqual(astunparse.unparse(ast1), astunparse.unparse(ast2))
 
     def check_roundtrip(self, code1, filename="internal", mode="exec"):
         ast1 = astunparse.loadastpy(str(code1), filename, mode)
