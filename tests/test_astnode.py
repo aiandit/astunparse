0cc29ce588e2a7411afa79cb885f021b0e9e4b98
Johannes Willkomm <johannes@johannes-willkomm.de>
2023-06-26 20:44:09 +0200

    Test for clone

--- tests/test_astnode.py
+++ tests/test_astnode.py
@@ -22,3 +22,11 @@ class UnparseTestCase(AstunparseCommonTestCase, unittest.TestCase):
         code2 = astunparse.unparse(ast1)
         ast2 = compile(code2, filename, mode, ast.PyCF_ONLY_AST)
         self.assertASTEqual(ast1, ast2)
+
+    def test_clone(self):
+        code = """
+def f(x):
+   r = x*x"""
+        ast1 = astunparse.loadastpy(code)
+        ast2 = ast1.clone()
+        astunparse.unparse(ast2)
